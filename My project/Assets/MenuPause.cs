d3fe6b1cb0fc9b935a3499fa0bd7b7e3995f2f68
Capitani Clavero Santiago <s.capitani@etec.local>
2023-06-22 10:39:55 -0300

    pause menu finished

--- My project/Assets/MenuPause.cs
+++ My project/Assets/MenuPause.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuPause : MonoBehaviour
 {
 
-    private GameObject botonPausa;
-    private GameObject menuPausa;
+    [SerializeField] private GameObject botonPausa;
+    [SerializeField] private GameObject menuPausa;
+    private bool pausedGame = false;
+
+    private void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if (pausedGame)
+            {
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+    }
 
    public void Pause ()
    {
+       pausedGame = true;
        Time.timeScale = 0f;
        botonPausa.SetActive(false);
        menuPausa.SetActive(true);
@@ -17,8 +32,22 @@ public class MenuPause : MonoBehaviour
 
    public void Resume()
    {
+       pausedGame = false;
        Time.timeScale = 1f;
        botonPausa.SetActive(true);
        menuPausa.SetActive(false);
    }
+
+   public void Reset ()
+    {
+        pausedGame = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Quit ()
+    {
+        Debug.Log("Juego Cerrado");
+        Application.Quit();
+    }
 }
