f90fc8803994746dd25756a6b17e452d36482a8a
Capitani Clavero Santiago <s.capitani@etec.local>
2023-08-28 12:08:21 -0300

    map extension, animation, jump, i need to fix the gravity (problem whit the map and character)

--- My project/Assets/MovementCharacter.cs
+++ My project/Assets/MovementCharacter.cs
@@ -5,19 +5,60 @@ using UnityEngine;
 public class MovementCharacter : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float jump;
     
     private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
+    public LayerMask Floor;
+
+    private bool lookright = true;
+    private Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         //Movimiento Horizontal
         float moveInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        if (moveInput != 0f)
+        {
+            animator.SetBool("isRunning", true);
+        }else
+        {
+            animator.SetBool("isRunning", false);
+        }
         
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        Orientacion(moveInput); 
+        JumpProcess();
+    }
+
+    //bool IsItOnFloor()
+    
+        //RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, new Vector2(boxCollider.bounds.size.x, boxCollider.bounds.size.y), 0f, Vector2.down, 0.2f, Floor);
+        //return raycastHit.collider ≠ null;
+
+    void JumpProcess()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+        }
+    }
+
+    void Orientacion(float moveInput)
+    {
+        if((lookright == true && moveInput < 0) || (lookright == false && moveInput > 0))
+        {
+            lookright = !lookright;
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
     }
 }
